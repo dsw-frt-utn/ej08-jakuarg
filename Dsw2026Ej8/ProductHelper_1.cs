fd582520413630c86421f514ee571140ee45a8a5
David Jacas Leila <David.JacasLeila@alu.frt.utn.edu.ar>
2026-05-02 20:39:59 -0300

    Problema1: Creación del método público ObtenerEtiquetaProducto.

---
+++ Dsw2026Ej8/ProductHelper_1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public partial class ProductHelper
+{
+    public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+    {
+        return $"[{code}] {description} - {FormatearMoneda(price)}";
+    }
+}
---
+++ Dsw2026Ej8/ProductHelper_2.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+public partial class ProductHelper
+{
+    private string FormatearMoneda(decimal valor) => valor.ToString("C", CultureInfo.CurrentCulture);
+}
