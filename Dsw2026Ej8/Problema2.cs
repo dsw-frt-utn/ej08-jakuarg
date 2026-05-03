768491f4c68d6e716fc946627904f305c5706648
David Jacas Leila <David.JacasLeila@alu.frt.utn.edu.ar>
2026-05-02 21:10:31 -0300

    Problema2: Creación del Método Público CrearResumenVenta

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Problema2
+{
+
+    public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+    {
+        decimal totalCalculado = 0;
+
+        if (quantity >= 0) { totalCalculado = quantity * unitPrice; }
+
+        var resumenVenta = new
+        {
+            Code = productCode,
+            Description = productDescription,
+            Quantity = quantity,
+            Total = totalCalculado
+        };
+
+        return $"{resumenVenta.Code} - {resumenVenta.Description} - {resumenVenta.Total:C2}";
+    }
+}
