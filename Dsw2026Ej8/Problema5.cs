d83ae6956b5cece9452b3edee2224dabd78f75a0
David Jacas Leila <David.JacasLeila@alu.frt.utn.edu.ar>
2026-05-03 13:04:22 -0300

    Problema5: fix: ObtenerImporteFinal

--- Dsw2026Ej8/Problema5.cs
+++ Dsw2026Ej8/Problema5.cs
@@ -1,26 +1,51 @@
-﻿using System;
-
+﻿/*
+ * Problema N° 5
+Crear un método público ObtenerImporteFinal, dentro de una clase Problema5, que
+reciba:
+● Sale sale
+Debe retornar un decimal.
+La clase base Sale debe tener un método virtual CalculateTotal.
+Deben existir al menos dos clases derivadas:
+● RetailSale
+● WholesaleSale
+Cada clase derivada debe redefinir el cálculo del total.
+Condiciones:
+● RetailSale retorna el importe sin descuento.
+● WholesaleSale aplica un descuento del 10%.
+● El método ObtenerImporteFinal debe llamar a sale.CalculateTotal().
+● La resolución debe demostrar que se ejecuta el método correspondiente según el
+tipo real del objeto recibido.
+ */
 public class Problema5
 {
     public decimal ObtenerImporteFinal(Sale sale)
     {
-
-        return 0;
+        return sale.CalculateTotal();
     }
 }
 
 public class Sale
 {
-    private virtual CalculateTotal()
-    {
+    public decimal Importe { get; set; }
 
+    public Sale(decimal importe)
+    {
+        Importe = importe;
+    }
+
+    public virtual decimal CalculateTotal()
+    {
+        return Importe;
     }
 }
 
 public class RetailSale : Sale
 {
-    public virtual CalculateTotal()
-    {
-
-    }
+    public RetailSale(decimal importe) : base(importe) { }
+    public override decimal CalculateTotal() => Importe;
+}
+public class WholesaleSale : Sale
+{
+    public WholesaleSale(decimal importe) : base(importe) { }
+    public override decimal CalculateTotal() => Importe * 0.90M;
 }
