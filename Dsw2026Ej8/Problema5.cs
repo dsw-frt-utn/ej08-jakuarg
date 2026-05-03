bbed3fbb2b678809619e79944966d60cdca82cbf
David Jacas Leila <David.JacasLeila@alu.frt.utn.edu.ar>
2026-05-03 12:56:18 -0300

    Problema5: ObtenerImporteFinal

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Problema5
+{
+    public decimal ObtenerImporteFinal(Sale sale)
+    {
+
+        return 0;
+    }
+}
+
+public class Sale
+{
+    private virtual CalculateTotal()
+    {
+
+    }
+}
+
+public class RetailSale : Sale
+{
+    public virtual CalculateTotal()
+    {
+
+    }
+}
